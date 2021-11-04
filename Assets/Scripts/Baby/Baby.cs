b66153996a035787246065a6b8c6607012ab70a7
ALOVasconcelos <alovasconcelos@gmailcom>
2021-11-04 20:23:16 -0300

    Implementado pulo do BabyZombie

--- Assets/Scripts/Baby/Baby.cs
+++ Assets/Scripts/Baby/Baby.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Baby : MonoBehaviour
 {
     public AudioSource groan;
+    private Rigidbody2D rigidBody;
+    public float jumpValue;
+    private bool isJumping = false;
 
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody2D>();
         groan.Play();
     }
 
@@ -19,6 +23,9 @@ public class Baby : MonoBehaviour
             float g = h > 0 ? 0 : 180;
             transform.rotation = Quaternion.Euler(new Vector3(0, g, 0));
             transform.Translate(new Vector3 (Mathf.Abs(h) * Time.deltaTime, 0, 0));
+        }
+        if(!isJumping && Input.GetButtonDown("Jump")) {
+            Jump();
         }
     }
 
@@ -32,5 +39,21 @@ public class Baby : MonoBehaviour
         ZombieControl();
         
     }
+
+    void Jump() {
+        rigidBody.AddForce(new Vector2(0f, jumpValue), ForceMode2D.Impulse);    
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer == 3) {
+            isJumping = false;
+        }
+    }
     
+    void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.layer == 3) {
+            isJumping = true;
+        }
+        
+    }
 }
