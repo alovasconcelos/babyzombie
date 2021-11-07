30f3d9aaac77c117508bb3723c150ab84d2bfb69
ALOVasconcelos <alovasconcelos@gmailcom>
2021-11-07 20:42:58 -0300

    ...

--- Assets/Scripts/Baby/Baby.cs
+++ Assets/Scripts/Baby/Baby.cs
@@ -6,14 +6,34 @@ public class Baby : MonoBehaviour
 {
     public AudioSource groan;
     public AudioSource jump;
-    private Rigidbody2D rigidBody;
     public float jumpValue;
+    
+
+    private Rigidbody2D rigidBody;
+    private Animator anim;
+    private Renderer rend;
+
     private bool isJumping = false;
+    public float blinkTime;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
         groan.Play();
+    }
+
+    IEnumerator Blink()
+    {
+        for (int i = 0; i < blinkTime; i++)
+        {
+            rend.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+            rend.enabled = false;
+            yield return new WaitForSeconds(0.1f);
+        }
+        rend.enabled = true;
     }
 
     void ZombieControl() 
@@ -21,11 +41,16 @@ public class Baby : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         
         if (h != 0) {
+            // The Baby Zombie is walking
             float g = h > 0 ? 0 : 180;
             transform.rotation = Quaternion.Euler(new Vector3(0, g, 0));
             transform.Translate(new Vector3 (Mathf.Abs(h) * Time.deltaTime, 0, 0));
+            
         }
-        if(!isJumping && Input.GetButtonDown("Jump")) {
+        //StartCoroutine(Blink());
+        anim.SetBool("isWalking", h != 0);
+
+        if (!isJumping && Input.GetButtonDown("Jump")) {
             Jump();
         }
     }
